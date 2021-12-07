5a43c909c3dd5432a90fd4d6a7795d4037d11775
TakeoutBubble <93976601+TakeoutBubble@users.noreply.github.com>
2021-12-07 19:15:49 +0700

    Lanjutan Fetch API : POST

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
-createApp(App).mount('#app');
+import router from './router'
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
